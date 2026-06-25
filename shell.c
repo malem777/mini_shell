dfd25468a8ab288b78ae70077333499b68a8b910
ADMIN <malem02007@gmail.com>
2026-06-25 10:47:30 +0100

    feat: add built-in cd command fix: improve input handling

--- shell.c
+++ shell.c
@@ -14,20 +14,31 @@ int main(){
     user[strcspn(user, "\n")] = '\0';
 
     do{
-        printf("\nShell\\%s>", user);
-        fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = '\0';
-
+        do{
+            printf("\nShell\\%s>", user);
+            fgets(command, sizeof(command), stdin);
+            command[strcspn(command, "\n")] = '\0';
+        }while(command[0]!=='\0');
         if(strcmp(command, "exit") == 0){
             break;
         }
             
         int size = 0;
         char *token = strtok(command, " ");
-        char **cmds = NULL;
-        char **temp = NULL;
+        char direction; char *file = NULL;
+        char **cmds = NULL; char **temp = NULL;
 
         while(token != NULL){
+            if(strcmp(token, ">") == 0  ||  strcmp(token, "<") == 0){
+                if(strcmp(token , "<")==0){
+                    direction = '<';
+                }
+                if(strcmp(token, ">")==0){
+                    direction = '>';
+                }
+                file = strtok(NULL, " ");
+                break;
+            }
             if(size==0){
                 cmds = malloc(sizeof(char*));
                 if(!cmds){free(cmds); exit(1);}
@@ -45,22 +56,27 @@ int main(){
         if(!temp){free(cmds); exit(1);}
         cmds = temp;
         cmds[size] = NULL; 
-    
-        pid_t pid = fork();
 
-        if(pid<0){free(cmds); exit(1);}
-        if(pid==0){
-            execvp(cmds[0], cmds);
-            perror("execvp failed");
-            exit(1);
-        }
-        else{
-            wait(NULL);
+        if(strcmp(cmds[0], "cd") == 0){
+            if(cmds[1] != NULL){
+                if(chdir(cmds[1])<0){
+                    perror("cd");
+                }
+            }
             free(cmds);
+        }else{
+            pid_t pid = fork();
+            if(pid<0){free(cmds); exit(1);}
+            if(pid==0){
+                execvp(cmds[0], cmds);
+                perror("execvp");
+                exit(1);
+            }else{
+                wait(NULL);
+                free(cmds);
+            }
         }
-
     }while(1);
 
     return 0;
 }
-
